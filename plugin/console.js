abc437c74222c7f59dbaa9062abb1136a7a542cc
jason_zuo <jason_zuo@trendmicro.com>
2023-09-12 13:43:19 +0800

    add babel-plugin

--- plugin/console.js
+++ plugin/console.js
@@ -1,3 +1,9 @@
+/*
+ * @author: jason_zuo
+ * @LastEditors: jason_zuo
+ * @LastEditTime: 2023-09-12 10:54:05
+ * @FilePath: \babel-plugin\plugin\console.js
+ */
 module.exports = (babel) => {
     const { types: t } = babel;
     return {
@@ -6,8 +12,12 @@ module.exports = (babel) => {
                 path.node.value = 2;
             },
             VariableDeclarator(path){
-                console.log(path)
                 path.node.kind = 'const'
+            },
+            Identifier(path){
+                if(path.parent.type === 'VariableDeclarator'){
+                    path.node.name = 'b'
+                }
             }
         }
     }
--- src/test.js
+++ src/test.js
@@ -1,2 +1,8 @@
-var a = 2 ;
-console.log(2)
+/*
+ * @author: jason_zuo
+ * @LastEditors: jason_zuo
+ * @LastEditTime: 2023-09-12 10:05:55
+ * @FilePath: \babel-plugin\src\test.js
+ */
+var a = 5 ;
+console.log(a)
